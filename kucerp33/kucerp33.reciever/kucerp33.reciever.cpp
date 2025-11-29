2ebe6816c9e6cca6a1841abfbe7d6c2205f00ee1
PetrPraveky <p1p1kucera@seznam.cz>
2025-11-29 18:31:55 +0100

    Init commit for me :D, idk what I'm cooking rn

---
+++ kucerp33/kucerp33.reciever/kucerp33.reciever.cpp
@@ -0,0 +1,10 @@
+﻿// kucerp33.reciever.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Reciever Module Online\n";
+    std::cout << "Hello World!\n";
+}
---
+++ kucerp33/kucerp33.sender/kucerp33.sender.cpp
@@ -0,0 +1,10 @@
+﻿// kucerp33.sender.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Sender Module Online\n";
+    std::cout << "Hello World!\n";
+}
